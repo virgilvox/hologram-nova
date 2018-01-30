69eea3f6935187118939f24f205202789d0f9e2c
virgilvox <sqrtofsaturn@gmail.com>
2018-01-30 15:55:42 -0700

    works with AT commands that have single line responses

--- examples/check_hardware.js
+++ examples/check_hardware.js
@@ -1,10 +1,15 @@
 var HologramNova = require("../index.js");
 var nova = new HologramNova({});
 
-nova.sendCommand("AT+CGMI\r\n", 25, null, null, function(res, err){
+nova.sendCommand("AT+CPIN?\r\n", 25, null, null, function(res, err){
+  if(err){
+    throw(err)
+  }
   console.log(res, err);
-});
-
-nova.sendCommand("AT+CPIN\r\n", 25, null, null, function(res, err){
-  console.log(res, err);
+  nova.sendCommand("AT+CGMI\r\n", 25, null, null, function(res, err){
+    if(err){
+      throw(err)
+    }
+    console.log(res, err);
+  });
 });
